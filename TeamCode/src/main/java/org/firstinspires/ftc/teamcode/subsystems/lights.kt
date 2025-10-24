443510ca5b5ae8a4cb01d98df99a1887f953503f
PythonAtSea <HudsonGreen27@gmail.com>
2025-10-23 21:23:02 -0400

    fix: prevent self destruction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/lights.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/lights.kt
@@ -30,8 +30,8 @@ object Lights : Subsystem {
             return
         }
         if (state == LightsState.IDLE) {
-            right.position += 0.02
-            left.position += 0.02
+            right.position += 0.002
+            left.position += 0.002
             if (right.position > 0.71) {
                 right.position = 0.28
             }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/turret.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/turret.kt
@@ -5,7 +5,7 @@ import dev.nextftc.core.subsystems.Subsystem
 import dev.nextftc.hardware.impl.MotorEx
 
 object Turret : Subsystem {
-    val motor = MotorEx("turret").brakeMode()
+    val motor = MotorEx("turret").zeroed().brakeMode()
     val encoder = MotorEx("frontLeft")
     var angle = 0.0
     var power = 0.0
@@ -18,9 +18,9 @@ object Turret : Subsystem {
     fun setPower(power: Double) = LambdaCommand("setTurretPower")
         .setStart {
             if (angle >= 19_000) {
-                this.power = -0.2
-            } else if (angle <= -19_000) {
                 this.power = 0.2
+            } else if (angle <= -19_000) {
+                this.power = -0.2
             } else {
                 this.power = power
             }
