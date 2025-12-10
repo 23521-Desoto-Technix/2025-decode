a95ee517eedbdb0b7fb29ea75530b0a1ba2b5b08
PythonAtSea <HudsonGreen27@gmail.com>
2025-12-10 14:22:27 -0500

    feat: make PTO test have ajustable power

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ptoTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ptoTest.kt
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.tests
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.DcMotorSimple
 
 @TeleOp(name = "PTO Test")
 class ptoTest : LinearOpMode() {
@@ -9,10 +10,17 @@ class ptoTest : LinearOpMode() {
         val pto = hardwareMap.servo["pto"]
         val left = hardwareMap.dcMotor["left"]
         val right = hardwareMap.dcMotor["right"]
+        right.direction = DcMotorSimple.Direction.REVERSE
+        var power = 0.0
         waitForStart()
         while (opModeIsActive()) {
-            left.power = -gamepad1.left_stick_y.toDouble()
-            right.power = -gamepad1.right_stick_y.toDouble()
+            left.power = power
+            right.power = power
+            if (gamepad1.leftBumperWasPressed()) {
+                power += 0.1
+            } else if (gamepad1.rightBumperWasPressed()) {
+                power -= 0.1
+            }
             if (gamepad1.a) {
                 pto.position = 0.0
             } else if (gamepad1.b) {
