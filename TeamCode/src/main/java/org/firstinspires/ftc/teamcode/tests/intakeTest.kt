d0eae6e23766cf646bb9f79ece5b26224a69c895
PythonAtSea <HudsonGreen27@gmail.com>
2025-12-19 15:18:58 -0500

    feat: intake test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/intakeTest.kt
@@ -0,0 +1,16 @@
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.DcMotorSimple
+
+@TeleOp(name = "Intake Test")
+class intakeTest : LinearOpMode() {
+    override fun runOpMode() {
+        val front = hardwareMap.dcMotor["frontIntake"]
+        val back = hardwareMap.dcMotor["backIntake"]
+        waitForStart()
+        while (opModeIsActive()) {
+            front.power = gamepad1.left_stick_y.toDouble()
+            back.power = gamepad1.right_stick_y.toDouble()
+        }
+    }
+}
