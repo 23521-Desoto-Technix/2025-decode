b59cb97f3c53599e6408880e2b7d63308d620af1
PythonAtSea <HudsonGreen27@gmail.com>
2025-12-21 20:34:25 -0500

    fix: access hardware correctly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/tube.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/tube.kt
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.subsystems
 
+import com.qualcomm.robotcore.hardware.DigitalChannel
 import dev.nextftc.core.commands.delays.WaitUntil
 import dev.nextftc.core.commands.groups.ParallelGroup
 import dev.nextftc.core.commands.utility.InstantCommand
@@ -10,14 +11,17 @@ import dev.nextftc.hardware.impl.MotorEx
 object Tube : Subsystem {
   val intake = MotorEx("intake")
   val transfer = MotorEx("transfer")
-  val top = ActiveOpMode.hardwareMap.digitalChannel["top"]
-  val middle = ActiveOpMode.hardwareMap.digitalChannel["middle"]
-  val bottom = ActiveOpMode.hardwareMap.digitalChannel["bottom"]
+  lateinit var top: DigitalChannel
+  lateinit var middle: DigitalChannel
+  lateinit var bottom: DigitalChannel
 
-  init {
-    top.mode = com.qualcomm.robotcore.hardware.DigitalChannel.Mode.INPUT
-    middle.mode = com.qualcomm.robotcore.hardware.DigitalChannel.Mode.INPUT
-    bottom.mode = com.qualcomm.robotcore.hardware.DigitalChannel.Mode.INPUT
+  override fun initialize() {
+    top = ActiveOpMode.hardwareMap.digitalChannel["top"]
+    middle = ActiveOpMode.hardwareMap.digitalChannel["middle"]
+    bottom = ActiveOpMode.hardwareMap.digitalChannel["bottom"]
+    top.mode = DigitalChannel.Mode.INPUT
+    middle.mode = DigitalChannel.Mode.INPUT
+    bottom.mode = DigitalChannel.Mode.INPUT
   }
 
   val intakeAll =
