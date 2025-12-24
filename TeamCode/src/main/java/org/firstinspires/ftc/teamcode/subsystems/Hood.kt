5666f7767c8a2eddbb1f4026edf713c98df1756d
PythonAtSea <HudsonGreen27@gmail.com>
2025-12-23 21:54:02 -0500

    fix: well that previous commit was goofy

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Hood.kt
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.subsystems
+
+import dev.nextftc.core.commands.utility.InstantCommand
+import dev.nextftc.core.subsystems.Subsystem
+import dev.nextftc.hardware.impl.ServoEx
+
+object Hood : Subsystem {
+  private val servo = ServoEx("hood")
+  private var position = 0.5
+  private const val BUMP_AMOUNT = 0.25
+
+  override fun initialize() {
+    servo.position = position
+  }
+
+  override fun periodic() {}
+
+  fun setPosition(pos: Double) = InstantCommand {
+    position = pos.coerceIn(0.0, 1.0)
+    servo.position = position
+  }
+
+  fun bumpUp() = InstantCommand {
+    position = (position + BUMP_AMOUNT).coerceIn(0.0, 1.0)
+    servo.position = position
+  }
+
+  fun bumpDown() = InstantCommand {
+    position = (position - BUMP_AMOUNT).coerceIn(0.0, 1.0)
+    servo.position = position
+  }
+}
