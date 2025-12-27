cff90a11867a114b4ce6c911493c2a1de34b02bd
PythonAtSea <HudsonGreen27@gmail.com>
2025-12-26 20:43:45 -0500

    fix: make PTO test not be broken

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ptoTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ptoTest.kt
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.tests
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
-import com.qualcomm.robotcore.hardware.DcMotorSimple
 
 @TeleOp(name = "PTO Test")
 class ptoTest : LinearOpMode() {
@@ -11,12 +10,7 @@ class ptoTest : LinearOpMode() {
     val frontLeft = hardwareMap.dcMotor["frontLeft"]
     val backLeft = hardwareMap.dcMotor["backLeft"]
     val frontRight = hardwareMap.dcMotor["frontRight"]
-
     val pto = hardwareMap.servo["pto"]
-    val left = hardwareMap.dcMotor["left"]
-    val right = hardwareMap.dcMotor["right"]
-    right.direction = DcMotorSimple.Direction.REVERSE
-    var power = 0.0
     waitForStart()
     while (opModeIsActive()) {
 
@@ -26,13 +20,6 @@ class ptoTest : LinearOpMode() {
       backLeft.power = -stickValue
       frontRight.power = -stickValue
 
-      left.power = power
-      right.power = power
-      if (gamepad1.leftBumperWasPressed()) {
-        power += 0.1
-      } else if (gamepad1.rightBumperWasPressed()) {
-        power -= 0.1
-      }
       if (gamepad1.a) {
         pto.position = 0.0
       } else if (gamepad1.b) {
