7d3a8bd9fa384f8a76a284b8f56c5bde11b80d46
PythonAtSea <HudsonGreen27@gmail.com>
2026-02-16 16:30:06 -0500

    fix(auto): actually go to the soft limit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/AutoConstants.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/AutoConstants.kt
@@ -28,7 +28,7 @@ object AutoConstants {
           pose("fastPark", Pose(93.0, 77.0, -45.0.deg.inRad))
           pose("s2Ctrl", Pose(85.0, 55.0, 0.0))
           pose("s3Ctrl", Pose(85.0, 30.0, 0.0))
-          pose("startFar", Pose(79.2, 7.8, 90.0.deg.inRad))
+          pose("startFar", Pose(88.9, 7.8, 90.0.deg.inRad))
           pose("shootFar", Pose(85.0, 15.0, 0.0.deg.inRad))
         }
 
@@ -60,8 +60,8 @@ object AutoConstants {
         linkedMapOf<String, Angle>().apply {
           angle("closeTurretRed", (-90.0).deg)
           angle("closeTurretBlue", 90.0.deg)
-          angle("farTurretRed", (-115.0).deg)
-          angle("farTurretBlue", 115.0.deg)
+          angle("farTurretRed", (-120.0).deg)
+          angle("farTurretBlue", 120.0.deg)
         }
 
     operator fun get(name: String): Angle = angles.getValue(name)
