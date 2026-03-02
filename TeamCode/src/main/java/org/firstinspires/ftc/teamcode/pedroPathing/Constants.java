64c06db33e1b79845156ee2f5fc58494ca46c652
dragonher0 <142940920+dragonher0@users.noreply.github.com>
2026-03-02 14:54:29 -0600

    feat: drive pid p value and gate delays for 18 solo close

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -26,7 +26,7 @@ public class Constants {
             .secondaryTranslationalPIDFCoefficients(new PIDFCoefficients(0.4, 0, 0.02, 0.03))
             .useSecondaryTranslationalPIDF(true)
             .headingPIDFCoefficients(new PIDFCoefficients(2, 0, 0.1, 0))
-            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.007, 0, 0.0003, 0.6, 0))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.009, 0, 0.0003, 0.6, 0))
             .secondaryDrivePIDFCoefficients(new FilteredPIDFCoefficients(0.01, 0, 0.0005, 0.6, 0));
 
     public static MecanumConstants driveConstants = new MecanumConstants()
