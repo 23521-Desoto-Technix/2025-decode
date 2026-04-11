4d4cd024a7118dfd08dbcd17fc258d3d9aafa4ed
PythonAtSea <HudsonGreen27@gmail.com>
2026-04-11 16:22:11 -0400

    feat(tests): add second middle breakbeam to breakbeam test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/BreakbeamDeviceTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/BreakbeamDeviceTest.kt
@@ -6,15 +6,19 @@ import dev.nextftc.ftc.NextFTCOpMode
 @TeleOp
 class BreakbeamDeviceTest : NextFTCOpMode() {
     private lateinit var topDigitalDevice: com.qualcomm.robotcore.hardware.DigitalChannel
-    private lateinit var middleDigitalDevice: com.qualcomm.robotcore.hardware.DigitalChannel
+    private lateinit var middleDigitalDeviceA: com.qualcomm.robotcore.hardware.DigitalChannel
+    private lateinit var middleDigitalDeviceB: com.qualcomm.robotcore.hardware.DigitalChannel
     private lateinit var bottomDigitalDevice: com.qualcomm.robotcore.hardware.DigitalChannel
 
     override fun onInit() {
         topDigitalDevice = hardwareMap.digitalChannel["top"]
         topDigitalDevice.mode = com.qualcomm.robotcore.hardware.DigitalChannel.Mode.INPUT
 
-        middleDigitalDevice = hardwareMap.digitalChannel["middle"]
-        middleDigitalDevice.mode = com.qualcomm.robotcore.hardware.DigitalChannel.Mode.INPUT
+        middleDigitalDeviceA = hardwareMap.digitalChannel["middleA"]
+        middleDigitalDeviceA.mode = com.qualcomm.robotcore.hardware.DigitalChannel.Mode.INPUT
+
+        middleDigitalDeviceB = hardwareMap.digitalChannel["middleB"]
+        middleDigitalDeviceB.mode = com.qualcomm.robotcore.hardware.DigitalChannel.Mode.INPUT
 
         bottomDigitalDevice = hardwareMap.digitalChannel["bottom"]
         bottomDigitalDevice.mode = com.qualcomm.robotcore.hardware.DigitalChannel.Mode.INPUT
@@ -22,7 +26,8 @@ class BreakbeamDeviceTest : NextFTCOpMode() {
 
     override fun onUpdate() {
         telemetry.addData("Top Digital State", topDigitalDevice.state)
-        telemetry.addData("Middle Digital State", middleDigitalDevice.state)
+        telemetry.addData("Middle Alpha Digital State", middleDigitalDeviceA.state)
+        telemetry.addData("Middle Beta Digital State", middleDigitalDeviceB.state)
         telemetry.addData("Bottom Digital State", bottomDigitalDevice.state)
         telemetry.update()
     }
