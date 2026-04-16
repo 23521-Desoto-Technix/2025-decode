45b77b76eb40877476d91e476497209f358ba918
PythonAtSea <HudsonGreen27@gmail.com>
2026-04-16 19:01:28 -0400

    fix(auto): actually use the farzone turret angles

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/far21push.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/far21push.kt
@@ -60,8 +60,8 @@ class far21push : NextFTCOpMode() {
     private fun buildRoutine(paths: Map<String, PathChain>): Command {
         val farTurretAngle =
             when (BotState.alliance) {
-                Alliance.RED -> AutoConstants.Angles["middleTurretRed"]
-                Alliance.BLUE -> AutoConstants.Angles["middleTurretBlue"]
+                Alliance.RED -> AutoConstants.Angles["farTurretRed"]
+                Alliance.BLUE -> AutoConstants.Angles["farTurretBlue"]
                 else -> 0.0.deg
             }
         val parkTurretAngle =
