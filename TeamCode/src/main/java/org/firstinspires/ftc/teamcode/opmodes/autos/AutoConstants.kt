5af43fc975ba987fc4c7a1ab3810f8e48d4f9937
PythonAtSea <HudsonGreen27@gmail.com>
2026-04-16 19:26:34 -0400

    feat(auto): spike3 + hp intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/AutoConstants.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/AutoConstants.kt
@@ -40,8 +40,9 @@ object AutoConstants {
                 pose("spike3Start", Pose(105.0, 36.0, 0.0.deg.inRad))
                 pose("spike3End", Pose(120.0, 36.0, 0.0.deg.inRad))
                 pose("spike3Ctrl", Pose(90.0, 35.0, 0.0.deg.inRad))
-                pose("humanIntake", Pose(130.0, 7.0, 0.0.deg.inRad))
-                pose("humanIntakeCtrl", Pose(89.0, 7.0, 0.0.deg.inRad))
+                pose("spike3CtrlFar", Pose(85.0, 35.0, 0.0.deg.inRad))
+                pose("humanIntake", Pose(130.0, 8.5, 0.0.deg.inRad))
+                pose("humanIntakeCtrl", Pose(89.0, 8.5, 0.0.deg.inRad))
                 pose("parkFar", Pose(110.0, 20.0, 0.0.deg.inRad))
             }
 
@@ -300,6 +301,34 @@ object AutoConstants {
                 follower
                     .pathBuilder()
                     .addPath(BezierLine(p("startFar"), p("push")))
+                    .setConstantHeadingInterpolation(p("startFar").heading)
+                    .build(),
+            )
+            path(
+                "startFarToShootFar",
+                follower
+                    .pathBuilder()
+                    .addPath(BezierLine(p("startFar"), p("shootFar")))
+                    .setConstantHeadingInterpolation(p("startFar").heading)
+                    .build(),
+            )
+            path(
+                "humanIntakeCombined",
+                follower
+                    .pathBuilder()
+                    .addPath(BezierCurve(p("shootFar"), p("humanIntakeCtrl"), p("humanIntake")))
+                    .setConstantHeadingInterpolation(p("startFar").heading)
+                    .addPath(BezierLine(p("humanIntake"), p("shootFar")))
+                    .setConstantHeadingInterpolation(p("startFar").heading)
+                    .build(),
+            )
+            path(
+                "shootFarToSpike3Combined",
+                follower
+                    .pathBuilder()
+                    .addPath(BezierCurve(p("shootFar"), p("spike3CtrlFar"), p("spike3End")))
+                    .setConstantHeadingInterpolation(p("startFar").heading)
+                    .addPath(BezierLine(p("spike3End"), p("shootFar")))
                     .setConstantHeadingInterpolation(p("startFar").heading)
                     .build(),
             )
