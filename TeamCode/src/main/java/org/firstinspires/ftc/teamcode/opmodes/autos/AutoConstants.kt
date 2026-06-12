28b9c4d0275a28604bdf44dff2f7be71704f8fd6
PythonAtSea <HudsonGreen27@gmail.com>
2026-06-12 19:28:14 -0400

    fix(auto): make park be closer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/AutoConstants.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/AutoConstants.kt
@@ -24,7 +24,7 @@ object AutoConstants {
                 pose("shootMiddle", Pose(88.0, 80.0, 0.0.deg.inRad))
                 pose("shootFar", Pose(85.0, 22.0, 0.0.deg.inRad))
                 pose("parkFar", Pose(88.0, 22.0, 0.0.deg.inRad))
-                pose("parkNear", Pose(110.0, 70.0, 0.0.deg.inRad))
+                pose("parkNear", Pose(100.0, 74.0, 0.0.deg.inRad))
                 pose("push", Pose(90.0, 8.5, 0.0.deg.inRad))
                 pose("shootPark", Pose(85.0, 105.0, 0.0.deg.inRad))
                 pose("sideSpike1", Pose(119.0, 88.0, -90.0.deg.inRad))
