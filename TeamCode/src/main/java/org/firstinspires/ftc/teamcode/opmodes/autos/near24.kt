27bb7ad52f29249a2cd3399640e26a2a8597caf5
PythonAtSea <HudsonGreen27@gmail.com>
2026-06-23 19:09:19 -0400

    tune(auto): change defaults

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/near24.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autos/near24.kt
@@ -20,7 +20,6 @@ import dev.nextftc.core.units.rad
 import dev.nextftc.extensions.pedro.FollowPath
 import dev.nextftc.extensions.pedro.PedroComponent
 import dev.nextftc.ftc.NextFTCOpMode
-import kotlin.time.Duration.Companion.milliseconds
 import org.firstinspires.ftc.robotcore.external.Telemetry
 import org.firstinspires.ftc.teamcode.TelemetryImplUpstreamSubmission
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants
@@ -33,6 +32,7 @@ import org.firstinspires.ftc.teamcode.utils.BotState
 import org.firstinspires.ftc.teamcode.utils.HtmlTelemetryUtils
 import org.firstinspires.ftc.teamcode.utils.ShootingConfigInterpolator
 import org.firstinspires.ftc.teamcode.utils.calculateTargetMetrics
+import kotlin.time.Duration.Companion.milliseconds
 
 @Autonomous(name = "Near 24", group = "Near", preselectTeleOp = "teleop")
 class near24 : NextFTCOpMode() {
@@ -52,9 +52,9 @@ class near24 : NextFTCOpMode() {
 
     var forceCurrent = true
 
-    var withPartner = false
+    var withPartner = true
 
-    var doPark = false
+    var doPark = true
 
     var gateBonk = false
 
@@ -129,10 +129,7 @@ class near24 : NextFTCOpMode() {
             ),
             FollowPath(paths.getValue("spike2ToShootMiddle")),
             Tube.shootAll(),
-            IfElseCommand(
-                { !do27 },
-                Delay(200.milliseconds),
-            ),
+            Delay(200.milliseconds),
             gateIntake,
             gateIntake,
             intake(FollowPath(paths.getValue("spike1Combined"))),
